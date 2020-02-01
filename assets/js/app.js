2b1672116efeed84eeb944a8c94563677f9f205d
Corentin Foucrier <foucrier.corentin@gmail.com>
2020-02-01 15:13:54 +0100

    refaco js; display x,y position system

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,72 @@
-const divTableBody = document.getElementById("divTableBody");
-
-let table = "";
+/* VARIABLES */
+const coords = document.getElementById("coords");
+const startButton = document.getElementById("start");
+const restartButton = document.getElementById("restart");
 const totalSquares = 8;
-
-for (let i = 0; i < totalSquares; i++) {
-    table += `<div class="divTableRow">\n`;
-    for (let j = 0; j < totalSquares; j++) {
-        table += `    <div class="divTableCell" id="${i}x${j}"></div>\n`;
-    }
-    table += `</div>\n`;
-}
-
-divTableBody.innerHTML = table;
-
-let boatsPos = [];
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const boatsProperties = { // Determine total of boats and size for each one.
     "totalOfBoats": 3,
     "boatsSizes": [4, 3, 2]
 };
-let boatsLeft = boatsProperties.totalOfBoats;
+let boatsPos = [];
 let tempoBoatsPos = [];
-const randomNb = interval => Math.floor(Math.random() * Math.floor(interval));
+let boatsLeft = boatsProperties.totalOfBoats;
+/* VARIABLES */
 
+const createBoard = () => {
+    let boardHMLT = "";
+
+    /* Y Posision display */
+    boardHMLT += `<div class="y">\n`;
+    boardHMLT += `    <div class="divTable">\n`;
+    boardHMLT += `        <div class="divTableRow">\n`;
+    for (let i = 0; i < totalSquares; i++) {
+        boardHMLT += `            <div class="divTableCell">${letters[i]}</div>\n`;
+    }
+    boardHMLT += `        </div>\n`;
+    boardHMLT += `    </div>\n`;
+    boardHMLT += `</div>\n`;
+    /* Y Posision display */
+
+    /* X Posision display */
+    boardHMLT += `<div class="x">\n`;
+    boardHMLT += `    <div class="divTable">\n`;
+    for (let i = 0; i < totalSquares; i++) {
+        boardHMLT += `        <div class="divTableRow">\n`;
+        boardHMLT += `            <div class="divTableCell">${i + 1}</div>\n`;
+        boardHMLT += `        </div>\n`;
+    }
+    boardHMLT += `    </div>\n`;
+    boardHMLT += `</div>\n`;
+    /* X Posision display */
+
+    /* Table display */
+    boardHMLT += `<div class="divTable">`;
+    boardHMLT += `    <div class="divTableBody">`;
+    for (let i = 0; i < totalSquares; i++) {
+        boardHMLT += `    <div class="divTableRow">\n`;
+        for (let j = 0; j < totalSquares; j++) {
+            boardHMLT += `            <div class="divTableCell" id="${i}x${j}"></div>\n`;
+        }
+        boardHMLT += `    </div>\n`;
+    }
+    boardHMLT += `    </div>\n`;
+    boardHMLT += `</div>\n`;
+    /* Table display */
+
+    coords.innerHTML = boardHMLT; // Put the result in HTML
+}
+
+/**
+ * Generate a random number between 0 and max
+ * @param {Number} max
+ */
+const randomNb = max => Math.floor(Math.random() * Math.floor(max));
 
 const startGame = () => {
+    startButton.classList.add("d-none");
+    restartButton.classList.remove("d-none");
+    createBoard();
     const debugTime0 = performance.now();
     const tob = boatsProperties.totalOfBoats;
     for (let i = 0; i < tob; i++) {
@@ -89,13 +132,11 @@ const buildBoat = (x, y, axis) => {
     }
     // console.log("after while");
     if (i !== currentBoatSize) {
-        tempoBoatsPos = [];
+        tempoBoatsPos = []; // Empty temporary positions array
         generateBoat();
     } else {
         // console.log(tempoBoatsPos);
         boatsPos = tempoBoatsPos.concat(boatsPos);
-        tempoBoatsPos = [];
+        tempoBoatsPos = []; // Empty temporary positions array
     }
 }
-
-startGame();
